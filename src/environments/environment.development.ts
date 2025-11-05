d4e7ed94979d8148e3134d44cf6d6a782378341b
SeynabouDiop <94376299+SeynabouDiop@users.noreply.github.com>
2025-11-05 00:16:48 +0000

    Update API URL for development environment

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://localhost:5000/api',
+  apiUrl: 'https://render-ges-immo.onrender.com/api',
   //apiUrl: 'https://votre-domaine.com/api',
   appName: 'Gestion Immobilière',
   version: '1.0.0'
